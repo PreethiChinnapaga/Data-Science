a472f87b6bffc7899ccee928af87559ae7f259c2
PreethiChinnapaga <90952830+PreethiChinnapaga@users.noreply.github.com>
2022-09-07 12:45:31 +0530

    Add files via upload

---
+++ Multiplication table from 1-10.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+# Multiplication table from 1-10
+for i in range(1,11):
+    for j in range(1,11):
+        print(str(i)+"*"+str(j)+"="+(str(i*j)))
+    print(" ")
+
